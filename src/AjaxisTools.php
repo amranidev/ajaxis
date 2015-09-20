655ee3eccb6a84d8bfd63d9c219daee3b57f10a7
Amrani <amranidev@gmail.com>
2015-09-20 16:42:51 +0100

    Mt checkbox&radio

--- src/AjaxisTools.php
+++ src/AjaxisTools.php
@@ -124,12 +124,13 @@ class AjaxisTools
                  <input type="range" name="' . $name . '" id = "' . $name . '" min="5" max="100" value = "' . $value . '" /></p>
                  <label for="' . $name . '" class="active">' . $label . '</label>';
         } 
-        elseif ($type == 'date') {
-            $l = '<input name = "' . $name . '" type="date" class="datepicker"><label for="' . $name . '" class="active">' . $label . '</label>';
+        } elseif($type == 'checkbox'){
+                        $l = '<p><input  id = "'.$name.'" name="' . $name . '" type="' . $type . '"  '.$value.'>
+                       <label for="' . $name . '">' . $label . '</label></p>';
             return $l;
-        } 
+        }
         else {
-            $l = '<input  name="' . $name . '" type="' . $type . '" class = "validate" value = "' . $value . '">
+            $l = '<input  id = "'.$name.'" name="' . $name . '" type="' . $type . '" class = "validate" value = "' . $value . '">
                        <label for="' . $name . '" class="active">' . $label . '</label>';
             return $l;
         }
