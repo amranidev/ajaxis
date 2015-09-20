565cd441693e2101ca2eaddc4ec6ae21c98956ea
Amrani <amranidev@gmail.com>
2015-09-21 00:38:21 +0100

    Mtjs optimisation

--- public/js/AjaxisMaterialize.js
+++ public/js/AjaxisMaterialize.js
@@ -1,100 +1,67 @@
-$(document).ready(function() {
-     $('.modal-trigger').leanModal();
-     $('select').material_select();
- });
- //*************************************** Deleting *******************************************
- $('body').on('click', '.delete', function() {
-
-      tr = $(this).parent().parent();
-     $.ajax({
-         async: true,
-         type: 'get',
-         url: baseURL + $(this).data('link'),
-         success:function(response) {
-             $('.AjaxisModal').html(response);
-         }
-     })
- });
-      $('body').on('click', '.remove', function() {
-        var modal = $(this).parent().parent().parent();
-        modal.closeModal();
-         $.ajax({
-          async: true,
-             method: 'get',
-             url: baseURL + $(this).data('link'),
-             success:function(response) {
-                 window.location.replace(ModelApi);
-             }
-         });
-     });
-//*****************************************Editing*************************************************
- $("body").on('click', '.edit', function() {
-     $.ajax({
-        async: true,
-         method: 'get',
-         url: baseURL + $(this).data('link'),
-         success: function(response) {
-             $('.AjaxisModal').html(response);
-             $('select').material_select();
-             $('.datepicker').pickadate({ selectMonths: true,selectYears: 15 });
-         }
-     })
- })
- $(document).on("click", ".update", function(e) {
+$(document).on('ready', function() {
+    $('.modal-trigger').leanModal();
+});
+$(document).on('click', '.deletingModalClose', function() {
+    $(this).parent().parent().parent().closeModal();
+})
+$(document).on("click", ".closeModal", function() {
+    $(this).parent().parent().parent().parent().closeModal();
+});
+$(document).on('click', '.show', function() {
+    GET($(this).data('link'));
+})
+$('body').on('click', '.delete', function() {
+    GET($(this).data('link'));
+});
+$("body").on('click', '.edit', function() {
+    GET($(this).data('link'));
+})
+$(document).on("click", ".create", function() {
+    GET($(this).data('link'));
+})
+$(document).on("click", ".update", function(e) {
     e.preventDefault();
-     postData = $(this).parent().parent().serializeArray();
-     var close = $(this).parent().parent().parent().parent();
-     $.ajax({
+    POST($(this).parent().parent().serializeArray(), $(this).data('link'));
+});
+$(document).on("click", ".save", function() {
+    POST($(this).parent().parent().serializeArray(), $(this).data('link'));
+})
+
+$('body').on('click', '.remove', function() {
+    $.ajax({
         async: true,
-         type: 'post',
-         url: baseURL + $(this).data('link'),
-         data: postData,
-         success: function(response) {
-             window.location.replace(ModelApi);
-         }
-     });
- });
- $(document).on("click", ".closeModal", function() {
-     $(this).parent().parent().parent().parent().closeModal();
- });
- //******************************************** Creating ******************************************************
- $(document).on("click", ".create", function() {
-     $.ajax({
+        method: 'get',
+        url: baseURL + $(this).data('link'),
+        success: function(response) {
+            window.location.replace(response);
+        }
+    });
+});
+
+function POST(postData, dataLink) {
+    $.ajax({
         async: true,
-         type: 'get',
-         url: baseURL + $(this).data('link'),
-         success:function(response) {
+        type: 'post',
+        url: baseURL + dataLink,
+        data: postData,
+        success: function(response) {
+            window.location.replace(response);
+        }
+    })
+}
+
+function GET(dataLink) {
+    $.ajax({
+        async: true,
+        type: 'get',
+        url: baseURL + dataLink,
+        success: function(response) {
             $('.AjaxisModal').html(response);
             $('select').material_select();
-            $('.datepicker').pickadate({ selectMonths: true,selectYears: 15 });
-         }
-     })
- })
-      $(document).on("click", ".save", function() {
-         var postData = $(this).parent().parent().serializeArray();
-         //Materialize.toast('an element has been created!', 5000);
-         $.ajax({
-            async: true,
-             type: 'post',
-             url: baseURL + $(this).data('link'),
-             data: postData,
-             success:function(response) {
-                 window.location.replace(ModelApi);
-             }
-         })
-     })
- $(document).on('click', '.show', function() {
-     $.ajax({
-        async: true,
-         type: 'get',
-         url: baseURL + $(this).data('link') + $(this).data('id'),
-         success: function(response) {
-             $('.AjaxisModal').html(response);
-         }
-     })
- })
- $(document).on('click','.deletingModalClose',function(){
-
-    $(this).parent().parent().parent().closeModal();
- })
- 
+            $('.datepicker').pickadate({
+                selectMonths: true,
+                selectYears: 15
+            });
+        }
+    })
+}
