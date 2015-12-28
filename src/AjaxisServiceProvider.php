d3ae33d90dd6d13f49c406ef52868b9a237e2a59
Amrani <amranidev@gmail.com>
2015-12-28 23:34:19 +0000

    load Views

--- src/AjaxisServiceProvider.php
+++ src/AjaxisServiceProvider.php
@@ -4,20 +4,27 @@ namespace Amranidev\Ajaxis;
 use Amranidev\Ajaxis\AjaxisGenerate;
 use Illuminate\Support\ServiceProvider;
 
-class AjaxisServiceProvider extends ServiceProvider {
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register() {
-		$this->app->bind('Ajaxis', function () {
-			return new AjaxisGenerate();
-		});
-	}
-	public function boot() {
-		$this->publishes([
-			__DIR__ . '/../public' => public_path(),
-		], 'public');
-	}
+class AjaxisServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Ajaxis', function () {
+            return new AjaxisGenerate();
+        });
+    }
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../public' => public_path(),
+        ], 'public');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Ajaxis');
+    }
+
 }
