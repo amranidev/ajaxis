fe3d8bc15ab2172ec8b98508b6ccc71da0b95aff
Amrani <amranidev@gmail.com>
2015-12-28 23:35:01 +0000

    helper class test

--- src/AjaxisTools.php
+++ src/AjaxisTools.php
@@ -151,7 +151,9 @@ class AjaxisTools
               <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               <h4 class="modal-title" id="myModalLabel">' . $title . '</h4></div>
+
               <div class="modal-body">' . $body . '</div>
+
               <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               <button type="button" class="destroy btn btn-primary" data-link = "' . $link . '">Ok</button>
