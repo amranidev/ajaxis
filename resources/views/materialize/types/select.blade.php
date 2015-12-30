5c98034f1b74976edae4a9b98e3c3b5deaf67905
Amrani <amranidev@gmail.com>
2015-12-30 21:04:27 +0000

    check value if null

--- resources/views/materialize/types/select.blade.php
+++ resources/views/materialize/types/select.blade.php
@@ -1,10 +1,12 @@
 <div class="row">
     <div class="file-field input-field col s12">
-        <select>
+        <select name = "{{$name}}">
             <option value="" disabled selected>Choose your option</option>
+            @if($value)
             @foreach($value as $k => $v)
             <option value = '{{$k}}'>{{$v}}</option>
             @endforeach
+            @endif
         </select>
     </div>
 </div>
