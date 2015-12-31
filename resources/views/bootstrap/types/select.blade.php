664cb60332beb7145551a8cba419bf1f185c329f
Amrani <amranidev@gmail.com>
2015-12-31 23:22:08 +0000

    add name key to blade

--- resources/views/bootstrap/types/select.blade.php
+++ resources/views/bootstrap/types/select.blade.php
@@ -1,5 +1,5 @@
 <label class="control-label">{{$label}}</label>
-<select class="form-control">
+<select class="form-control" name = '{{$name}}'>
 	@if($value)
     @foreach ($value as $k => $v)
     <option value =  "{{$k}}">{{$v}}</option>
