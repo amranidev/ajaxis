d7aa02c1534584cf417b29281708cae14a4abd3f
Amrani <amranidev@gmail.com>
2016-01-01 17:01:21 +0000

    Ajaxis opt

--- src/Ajaxis.php
+++ src/Ajaxis.php
@@ -1,12 +1,19 @@
 <?php
+
 namespace Amranidev\Ajaxis;
+
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * class Ajaxis
+ *
+ * @package ajaxis
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 class Ajaxis extends Facade
 {
-    
-    protected static function getFacadeAccessor() {
+    protected static function getFacadeAccessor()
+    {
         return 'Amranidev\Ajaxis\AjaxisGenerate';
     }
 }
-
