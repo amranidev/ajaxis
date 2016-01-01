9fb8297f0ad80c1c9768923700391d926ba423bc
Amrani <amranidev@gmail.com>
2016-01-01 17:04:30 +0000

    MaterilizeDeleteConfirmationMessage opt

--- src/Materialize/Builders/MaterializeDeleteConfirmationMessage.php
+++ src/Materialize/Builders/MaterializeDeleteConfirmationMessage.php
@@ -5,30 +5,64 @@ namespace Amranidev\Ajaxis\Materialize\Builders;
 use Amranidev\Ajaxis\Modal\Modal;
 use Amranidev\Ajaxis\Modal\ModalInterface;
 
+/**
+ * class MaterializeDeleteConfirmationMessage
+ *
+ * @package ajaxis/Materialize/Builders
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 class MaterializeDeleteConfirmationMessage implements ModalInterface
 {
+    /**
+     * Modal Instance
+     *
+     * @var $Modal
+     */
     public $Modal;
 
+    /**
+     * Create new MaterializeDeleteConfirmationMessage
+     */
     public function __construct()
     {
         $this->Modal = new Modal();
     }
 
+    /**
+     * Build modal head
+     *
+     * @param $title String
+     */
     public function buildHead($title)
     {
         $this->Modal->modalHead = view('Ajaxis::materialize.get.head', compact('title'))->render();
     }
 
+    /**
+     * Build modal body
+     *
+     * @param $input Array
+     */
     public function buildBody($a, $b, $c, $input)
     {
         $this->Modal->modalBody .= $input;
     }
 
-    public function buildfooter($link, $action)
+    /**
+     * Build modal footer
+     *
+     * @param $link String
+     * @param $action String
+     */public function buildfooter($link, $action)
     {
         $this->Modal->modalFooter = view('Ajaxis::materialize.get.footer', compact('link', 'action'))->render();
     }
 
+    /**
+     * Get Modal instance
+     *
+     * @return Modal
+     */
     public function getResult()
     {
         return $this->Modal;
