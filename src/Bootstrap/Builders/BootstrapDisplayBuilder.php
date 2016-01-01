2d3752c5a3085b7a140de5981b2c67b07deacb63
Amrani <amranidev@gmail.com>
2016-01-01 17:03:18 +0000

    BootstrapDisplayBuilder opt

--- src/Bootstrap/Builders/BootstrapDisplayBuilder.php
+++ src/Bootstrap/Builders/BootstrapDisplayBuilder.php
@@ -5,31 +5,66 @@ namespace Amranidev\Ajaxis\Bootstrap\Builders;
 use Amranidev\Ajaxis\Modal\Modal;
 use Amranidev\Ajaxis\Modal\ModalInterface;
 
+/**
+ * class BootstrapDisplayBuilder
+ *
+ * @package ajaxis/Bootstrap/Builders
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 class BootstrapDisplayBuilder implements ModalInterface
 {
+    /**
+     * Modal Instance
+     *
+     * @var $Modal
+     */
     public $Modal;
 
+    /**
+     * Create new BootstrapDisplayBuilder instance
+     */
     public function __construct()
     {
         $this->Modal = new Modal();
     }
 
+    /**
+     * Build modal head
+     *
+     * @param $title String
+     */
     public function buildHead($title)
     {
         $this->Modal->modalHead = view('Ajaxis::bootstrap.get.head', compact('title'));
     }
 
+    /**
+     * Build modal body
+     *
+     * @param $input Array
+     */
     public function buildBody($a, $b, $c, $input)
     {
         $this->Modal->modalBody .= view('Ajaxis::bootstrap.display.body', compact('input'))->render();
 
     }
 
+    /**
+     * Build modal footer
+     *
+     * @param $link String
+     * @param $action String
+     */
     public function buildFooter($link, $action)
     {
         $this->Modal->modalFooter = '</div></div></div></div>';
     }
 
+    /**
+     * Get Modal instance
+     *
+     * @return Modal
+     */
     public function getResult()
     {
         return $this->Modal;
