ade862fab1ef4c29f12fd6626ecbd6f37245b0f2
Amrani <amranidev@gmail.com>
2016-01-01 17:05:01 +0000

    MaterializeDisplayBuilder opt

--- src/Materialize/Builders/MaterializeDisplayBuilder.php
+++ src/Materialize/Builders/MaterializeDisplayBuilder.php
@@ -5,30 +5,65 @@ namespace Amranidev\Ajaxis\Materialize\Builders;
 use Amranidev\Ajaxis\Modal\Modal;
 use Amranidev\Ajaxis\Modal\ModalInterface;
 
+/**
+ * class MAterializeDisplayBuilder
+ *
+ * @package ajaxis/Materialize/Builders
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 class MaterializeDisplayBuilder implements ModalInterface
 {
+    /**
+     * Modal Instance
+     *
+     * @var $Modal
+     */
     public $Modal;
 
+    /**
+     * Create new MaterializeDisplayBuilder instance
+     */
     public function __construct()
     {
         $this->Modal = new Modal();
     }
 
+    /**
+     * Build modal head
+     *
+     * @param $title String
+     */
     public function buildHead($title)
     {
         $this->Modal->modalHead = '';
     }
 
+    /**
+     * Build modal body
+     *
+     * @param $input Array
+     */
     public function buildBody($a, $b, $c, $input)
     {
         $this->Modal->modalBody .= view('Ajaxis::materialize.display.body', compact('input'))->render();
     }
 
+    /**
+     * Build modal footer
+     *
+     * @param $link String
+     * @param $action String
+     */
     public function buildFooter($link, $action)
     {
         $this->Modal->modalFooter = '';
     }
 
+    /**
+     * Get Modal instance
+     *
+     * @return Modal
+     */
     public function getResult()
     {
         return $this->Modal;
