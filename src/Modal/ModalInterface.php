04e8832d4cfec0025ca8e34ac49912dec371fe22
Amrani <amranidev@gmail.com>
2016-01-02 17:14:24 +0000

    ModalInterface opt

--- src/Modal/ModalInterface.php
+++ src/Modal/ModalInterface.php
@@ -2,10 +2,43 @@
 
 namespace Amranidev\Ajaxis\Modal;
 
+/**
+ * interface ModalInterface
+ *
+ * @package ajaxis\Modal
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 interface ModalInterface
 {
+    /**
+     * build head of a modal
+     *
+     * @param $title String
+     */
     public function buildHead($title);
+
+    /**
+     * build body of a modal
+     *
+     * @param $label String
+     * @param $name String
+     * @param $value String
+     * @param $type String
+     */
     public function buildBody($label, $name, $value, $type);
+
+    /**
+     * build footer of a modal
+     *
+     * @param $link String
+     * @param $action String
+     */
     public function buildFooter($link, $action);
+
+    /**
+     * get modal
+     *
+     * @return Modal
+     */
     public function getResult();
 }
