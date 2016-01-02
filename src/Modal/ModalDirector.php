cad5e93ef0552018d81d0c76fa90c0562dc63496
Amrani <amranidev@gmail.com>
2016-01-02 17:13:58 +0000

    ModalDirector.php

--- src/Modal/ModalDirector.php
+++ src/Modal/ModalDirector.php
@@ -4,15 +4,41 @@ namespace Amranidev\Ajaxis\Modal;
 
 use Amranidev\Ajaxis\Modal\ModalInterface;
 
+/**
+ * class ModalDirector
+ *
+ * @package ajaxis\Modal
+ * @author Amrani Houssain <amranidev@gmail.com>
+ */
 class ModalDirector
 {
 
+    /**
+     * build modal
+     *
+     * @param $title String
+     * @param $action String
+     * @param $input Array/String
+     * @param $link String
+     * @param $modal Modal
+     *
+     * $input may be a string or an array.
+     */
     public function build($title, $action, $input, $link, ModalInterface $modal)
     {
-
+        /**
+         * build modal head
+         */
         $modal->buildHead($title);
 
+        /**
+         * check if $input is an array
+         */
         if (is_array($input)) {
+
+            /**
+             * check if name key exists
+             */
             if (array_key_exists('name', $input[1])) {
                 foreach ($input as $val) {
                     $modal->buildBody($val['key'], $val['name'], $val['value'], $val['type']);
@@ -24,8 +50,14 @@ class ModalDirector
             $modal->buildBody(null, null, null, $input);
         }
 
+        /**
+         * build modal footer
+         */
         $modal->buildFooter($link, $action);
 
+        /**
+         * get modal building
+         */
         return $modal->getResult();
     }
 }
