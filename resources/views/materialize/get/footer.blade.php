9ea6f6c8cf2f85b7314c5a8171a1770adafeea99
Amrani <amranidev@gmail.com>
2016-01-03 22:10:53 +0000

    change class to close button

--- resources/views/materialize/get/footer.blade.php
+++ resources/views/materialize/get/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="modal-footer">
-    <a href = "#" class="deletingModalClose modal-action modal-close waves-effect waves-green btn-flat">close</a>
+    <a href = "#" class="closeModal modal-action modal-close waves-effect waves-green btn-flat">close</a>
     <a href = "#" class="waves-effect waves-green btn-flat remove" data-link = "{{$link}}">{{$action}}</a>
 </div>
 </div>
