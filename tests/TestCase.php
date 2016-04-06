f5f94ddf28225b5bc352f30f2782424c46f295d9
Amrani <amranidev@gmail.com>
2016-04-06 23:17:09 +0100

    update testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,25 +1,9 @@
 <?php
+namespace Amranidev\Ajaxis\Tests;
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__ . '/../bootstrap/app.php';
-
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
+    //
 }
