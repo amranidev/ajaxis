6ee7ce8c4dddf7a1b05ff5d20c8bd1b05b604b73
Amrani <amranidev@gmail.com>
2016-04-19 19:21:26 +0100

    creat the BootstrapText class

---
+++ src/Bootstrap/Builders/BootstrapText.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Amranidev\Ajaxis\Bootstrap\Builders;
+
+use Amranidev\Ajaxis\Modal\Modal;
+use Amranidev\Ajaxis\Modal\ModalInterface;
+
+/**
+ * Class BootstrapText
+ * 
+ * @author Amrani Houssain <amranidev@gmail.com
+ */
+class BootstrapText implements ModalInterface
+{
+    /**
+     * modal instance
+     *
+     * @var $Modal
+     */
+    public $Modal;
+
+    /**
+     * create new BootstrapDeleteConfitmationMessage instance
+     */
+    public function __construct()
+    {
+        $this->Modal = new Modal();
+    }
+
+    /**
+     * Build modal head
+     *
+     * @param $title String
+     */
+    public function buildHead($title)
+    {
+        $this->Modal->modalHead = view('Ajaxis::bootstrap.get.head', compact('title'))->render();
+    }
+
+    /**
+     * Build modal body
+     *
+     * @param $input Array
+     */
+    public function buildBody($a, $b, $c, $input)
+    {
+        $this->Modal->modalBody .= $input;
+    }
+
+    /**
+     * Build modal footer
+     *
+     * @param $link String
+     * @param $action String
+     */
+    public function buildFooter($link, $action)
+    {
+        $this->Modal->modalFooter = view('Ajaxis::bootstrap.get.footer', compact('link', 'action'))->render();
+    }
+
+    /**
+     * Get Modal instance
+     *
+     * @return Modal
+     */
+    public function getResult()
+    {
+        return $this->Modal;
+    }
+}
