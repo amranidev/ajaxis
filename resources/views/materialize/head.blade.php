ba4f6e6cd131bc2e60b2c0a8c5cfba8e40d83fbc
Amrani <amranidev@gmail.com>
2016-07-01 22:53:14 +0000

    support enctype Attribute

--- resources/views/materialize/head.blade.php
+++ resources/views/materialize/head.blade.php
@@ -1,3 +1,3 @@
-<form class="col s12"  method = "post">
+<form class="col s12"  method = "post" enctype="multipart/form-data" action = "{{$link}}">
     <input type = "hidden" name = "_token" value = "{{csrf_token()}}">
     <div class="modal-content"><h4> {{ $title }} </h4>
