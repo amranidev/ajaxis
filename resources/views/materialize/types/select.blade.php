ac15327fc2c7a578709826f5f3054c180e81c4e9
Amrani <amranidev@gmail.com>
2016-08-27 14:04:44 +0100

    update template

--- resources/views/materialize/types/select.blade.php
+++ resources/views/materialize/types/select.blade.php
@@ -1,6 +1,7 @@
 <div class="row">
     <div class="file-field input-field col s12">
-        <select name = "{{$name}}">
+        <label for="{{$name}}">{{$label}}</label>
+        <select id = "{{$name}}" name = "{{$name}}">
             <option value="" disabled selected>Choose your option</option>
             @if($value)
             @foreach($value as $v)
